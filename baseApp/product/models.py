f56f6e69d040ed539b878d29bf3de10a52ed2bcd
andrewzweb <andrewzweb@gmail.com>
2020-05-10 21:56:38 +0300

    (feat) create models Product and make migration
    + add app in admin
    + add model Product
    + make first migration this app

--- baseApp/product/models.py
+++ baseApp/product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.ImageField(upload_to='product')
+    title = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    
