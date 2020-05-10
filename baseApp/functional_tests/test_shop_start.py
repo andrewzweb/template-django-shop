759e25c1b23dea2fe77e5d80ab398c3d8a5a1eb6
andrewzweb <andrewzweb@gmail.com>
2020-05-10 17:45:49 +0300

    (test) in main page must be header, products, footer

--- baseApp/functional_tests/test_shop_start.py
+++ baseApp/functional_tests/test_shop_start.py
@@ -2,9 +2,20 @@ from .base import FunctionalTest
 
 class ShopShowStartPageTest(FunctionalTest):
 
-    def test_get_start_page(self):
+    def test_get_start_page_and_check_title(self):
         '''get start page '''
 
         self.browser.get(self.live_server_url)
         self.assertIn('Shop', self.browser.title)
+
+
+    def test_simple_page_has_main_element(self):
+        '''get start page '''
+
+        self.browser.get(self.live_server_url)
+        self.assertIn('Shop', self.browser.title)
+
+        self.browser.find_element_by_id('header')
+        self.browser.find_element_by_id('products')
+        self.browser.find_element_by_id('footer')
 
