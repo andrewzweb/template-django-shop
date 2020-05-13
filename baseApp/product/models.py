aa64f4e47478342f7e81256bfeacff09e17fb06d
andrewzweb <andrewzweb@gmail.com>
2020-05-13 19:30:26 +0300

    (feat) update models product

--- baseApp/product/models.py
+++ baseApp/product/models.py
@@ -4,9 +4,9 @@ from django.utils.text import slugify
 
 class Product(models.Model):
     image = models.ImageField(upload_to='product', blank=True, null=True)
-    title = models.CharField(max_length=200, blank=False, unique=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
-    slug = models.SlugField(default='', blank=True, null=True)
+    title = models.CharField(max_length=200, default='', blank=False, unique=True)
+    price = models.DecimalField(default=None, max_digits=5, decimal_places=2, blank=True, null=True)
+    slug = models.SlugField(default='', blank=True)
     
     def __str__(self):
         return "{}".format(self.title)
