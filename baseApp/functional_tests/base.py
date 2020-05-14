32b9a28ba59995eb26f197126c9523130d4210a0
andrewzweb <andrewzweb@gmail.com>
2020-05-15 01:42:08 +0300

    (refc) change FT make webdriver size minimum

--- baseApp/functional_tests/base.py
+++ baseApp/functional_tests/base.py
@@ -15,6 +15,7 @@ class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         ''' start firefox '''
         self.browser = webdriver.Firefox()
+        self.browser.set_window_size(800, 600)
         staging_server = os.environ.get('STAGING_SERVER')
         if staging_server: 
             self.live_server_url = 'http://' + staging_server
