b60aefa3f43989faab25e0029a7d90faa63db9ea
andrewzweb <andrewzweb@gmail.com>
2020-05-21 16:58:17 +0300

    (refc) refactor FT

--- baseApp/functional_tests/test_product_item.py
+++ baseApp/functional_tests/test_product_item.py
@@ -8,7 +8,9 @@ class ProductItemTest(FunctionalTest):
         ''' get product item '''
         
         slug_name = self.add_product(name='product1')
+
         self.browser.get(self.live_server_url + reverse('catalog:item', kwargs={'product_slug':slug_name}))
+
         self.wait_for(self.browser.find_elements_by_class_name('product-item')) 
 
 
--- baseApp/functional_tests/test_shop_start.py
+++ baseApp/functional_tests/test_shop_start.py
@@ -1,14 +1,12 @@
 from .base import FunctionalTest
 from django.urls import reverse 
-import time 
 
 class ShopShowStartPageTest(FunctionalTest):
 
     def test_get_start_page_and_check_title(self):
         '''get start page '''
-
         self.browser.get(self.live_server_url)
-        
+
         self.wait_for(self.browser.find_element_by_id('header'))
         self.wait_for(self.browser.find_element_by_id('footer'))
 
@@ -18,10 +16,10 @@ class ShopShowStartPageTest(FunctionalTest):
         ''' test get catalog product page'''
         self.add_product(name='one')
         self.add_product(name='two')
-        
+
         resp = self.browser.get(self.live_server_url)
         products = self.wait_for(self.browser.find_elements_by_class_name('product-item'))
-        
+
         assert len(products) == 2
 
 
