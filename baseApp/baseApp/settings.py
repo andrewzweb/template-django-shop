042a6e037f2aa10bc3cf4265347c68de1a9f086e
andrewzweb <andrewzweb@gmail.com>
2020-05-25 15:39:28 +0300

    (feat) add order view checkout
    + create app order
    + add order in apps
    + change urls in main.urls
    + make simple views

--- baseApp/baseApp/settings.py
+++ baseApp/baseApp/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'product',
     'cart',
     'account',
+    'order',
     'functional_tests',
 ]
 
--- baseApp/baseApp/urls.py
+++ baseApp/baseApp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('account/', include('account.urls', namespace='account')),
+    path('order/', include('order.urls', namespace='order')),
     path('', include('product.urls', namespace='catalog')),
 ] 
 
