3831c2fa70fa23c3c58a5460f473b951b9011363
andrewzweb <andrewzweb@gmail.com>
2020-05-25 19:02:38 +0300

    (feat) tie together order form, view and model

--- baseApp/order/views.py
+++ baseApp/order/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
 
 # Create your views here.
 
 def checkout(request):
-    return render(request, 'order/checkout.html')
+    cart = Cart(request)
+    form = OrderCreateForm()
+
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            # clear the cart
+            cart.clear()
+            return render(request, 'order/created.html', {'order': order})
+        else:
+            form = OrderCreateForm()
+    return render(request, 'order/checkout.html', {'cart': cart, 'form': form})
+
