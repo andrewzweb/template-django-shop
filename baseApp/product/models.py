8e9b29e4305c2a9ee7c4852079d5797a858e8bbc
andrewzweb <andrewzweb@gmail.com>
2020-05-25 17:40:26 +0300

    (fix) change product.models.price to more real

--- baseApp/product/models.py
+++ baseApp/product/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 class Product(models.Model):
     image = models.ImageField(upload_to='product', blank=True, null=True)
     title = models.CharField(max_length=200, default='', blank=False, unique=True)
-    price = models.DecimalField(default=None, max_digits=5, decimal_places=2, blank=True, null=True)
+    price = models.DecimalField(default=None, max_digits=10, decimal_places=2, blank=True, null=True)
     slug = models.SlugField(default='', blank=True)
     
     def __str__(self):
