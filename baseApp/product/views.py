9e167021523dfe290394d6b7ceba6b7893ea8c0f
andrewzweb <andrewzweb@gmail.com>
2020-05-31 23:38:23 +0300

    (feat) add using form

--- baseApp/product/views.py
+++ baseApp/product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import Product
 from cart.forms import CartAddProductForm, CartAddProductHideQuantityForm
+from .forms import AddProductForm
 
 def product_list(request):
     products = Product.objects.all()
@@ -14,13 +15,16 @@ def product_item(request, product_slug):
     return render(request, 'product/item.html', {'product': product, 'cart_product_form':cart_product_form})
 
 def product_add(request):
+    form = AddProductForm()
     if request.method == "POST":
         title = request.POST['title']
         price = request.POST['price']
-        
-        Product.objects.create(title=title, price=price)
+        form = AddProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
         return redirect(reverse('catalog:list'))
-    return render(request, 'product/item.html', {})
+    return render(request, 'product/add.html', {'form':form})
         
         
 
