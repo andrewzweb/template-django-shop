8663f7ddc5c8b83fa6d96d930056b93589e508a6
andrewzweb <andrewzweb@gmail.com>
2020-06-01 15:02:01 +0300

    (fix) fix error in test end create more test
    + add test product dont create without price
    + add fix all broken tests

--- baseApp/product/tests/test_views.py
+++ baseApp/product/tests/test_views.py
@@ -4,6 +4,9 @@ from django.urls import  reverse
 from product.models import Product
 from product.urls import *
 
+from django.utils.datastructures import MultiValueDictKeyError
+
+
 class HomePageTest(TestCase):
     
     def test_get_home_page(self):
@@ -20,16 +23,22 @@ class ProductItemTest(TestCase):
         resp = self.client.get(reverse('catalog:item', args=(product_url,))) 
         assert resp.status_code == 200
 
+    def test_default_products_item_dont_exist(self):
+        assert Product.objects.count() == 0
+
 class AddProductTest(TestCase):
     ''' add product tests'''
 
-    def test_add_product_post(self):
-        assert Product.objects.count() == 0
-        self.client.post(reverse('catalog:product_add'), {'title':'producsdf', 'price':2})
+    def test_add_product(self):
+        self.client.post(reverse('catalog:product_add'), {'title':'product1', 'price':2})
         assert Product.objects.count() == 1
 
-    def test_add_two_products(self):
+    def test_add_product_without_price_fail(self):
+        with self.assertRaises(MultiValueDictKeyError):
+            self.client.post(reverse('catalog:product_add'), {'title':'produc1',})
         assert Product.objects.count() == 0
+
+    def test_add_two_products(self):
         self.client.post(reverse('catalog:product_add'), {'title':'produc1', 'price':2})
         assert Product.objects.count() == 1
         self.client.post(reverse('catalog:product_add'), {'title':'product2', 'price':2})
